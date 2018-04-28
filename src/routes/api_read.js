41ded3c913bca3eaaee64fda5336c9bd1421b8a8
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-28 04:58:35 -0400

    Routes

--- src/routes/api_read.js
+++ src/routes/api_read.js
@@ -2,4 +2,12 @@ import axios from "axios"
 
 export default class ReadData {
     constructor(){ }
+
+    setPassengers = () => {
+        return axios.get("/api/get/passengers")
+    }
+
+    setBookings = () => {
+        return axios.get("/api/get/bookings")
+    }
 }
